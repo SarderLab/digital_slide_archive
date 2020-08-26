e0b338aec4299101b0798990d1704f1b78c4041d
Brendon Lutnick <brendonl@buffalo.edu>
2020-08-26 13:28:43 -0400

    Update deploy_docker.py

--- ansible/deploy_docker.py
+++ ansible/deploy_docker.py
@@ -54,7 +54,7 @@ ImageList = collections.OrderedDict([
         'pinned': 'v1.0.0',
     }),
     ('cli', {
-        'tag': 'brendonl/histomicstk-deeplab',
+        'tag': 'darshanagovind/histomicstk_build8',
         'pull': True,
         'pinned': 'v0.1.7',
     }),
